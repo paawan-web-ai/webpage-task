3dce5252d8da1cdfd0bbb5f302814b0cb3251ad9
paawan-web-ai <matrixescape345@gmail.com>
2026-09-10 17:35:52 +0530

    traveler

--- wander Webpage/src/App.jsx
+++ wander Webpage/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Header from "./Components/Header/Header";
+import Traveler from "./Components/TravelersChoose/Traveler";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <Traveler />
     </div>
   );
 };
---
+++ wander Webpage/src/Components/TravelersChoose/Traveler.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import "./Traveler.scss";
+const Traveler = () => {
+  return (
+    <div className="traveler-main">
+      <div className="left-main">
+        <div className="left-upper">
+          <h3>
+            Why Thousands of Travelers Choose WANDER.ph for their Philippine
+            Adventures
+          </h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            asperiores dolor, aspernatur quo cupiditate voluptates, dignissimos
+            voluptatem hic vero molestiae perferendis atque ullam quod?
+          </p>
+          <div className="left-upper-logo">
+            <i class="ri-instagram-line"></i>
+            <i class="ri-twitter-fill"></i>
+            <i class="ri-facebook-circle-fill"></i>
+          </div>
+        </div>
+        <div className="left-bottom">
+          <div className="left-main-bottom">
+            <div className="emoji">
+              <i class="ri-creative-commons-by-fill"></i>
+            </div>
+            <div className="emoji-para">
+              <h3>12k</h3>
+              <p>Happy and Satisfied Travel</p>
+            </div>
+          </div>
+          <div className="left-main-bottom">
+            <div className="emoji">
+              <i class="ri-creative-commons-by-fill"></i>
+            </div>
+            <div className="emoji-para">
+              <h3>12k</h3>
+              <p>Happy and Satisfied Travel</p>
+            </div>
+          </div>
+          <div className="left-main-bottom">
+            <div className="emoji">
+              <i class="ri-creative-commons-by-fill"></i>
+            </div>
+            <div className="emoji-para">
+              <h3>12k</h3>
+              <p>Happy and Satisfied Travel</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="right-main">
+        <div className="card-main">
+          <div className="image-card">
+            <img
+              src="https://images.unsplash.com/photo-1700087151950-d469682c28b7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+          </div>
+          <div className="image-para">
+            <h2>Local Experties</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              fuga saepe accusantium debitis aperiam atque.
+            </p>
+          </div>
+        </div>
+        <div className="card-main">
+          <div className="image-card">
+            <img
+              src="https://images.unsplash.com/photo-1700087151950-d469682c28b7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+          </div>
+          <div className="image-para">
+            <h2>Local Experties</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              fuga saepe accusantium debitis aperiam atque.
+            </p>
+          </div>
+        </div>
+        <div className="card-main">
+          <div className="image-card">
+            <img
+              src="https://images.unsplash.com/photo-1700087151950-d469682c28b7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+          </div>
+          <div className="image-para">
+            <h2>Local Experties</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              fuga saepe accusantium debitis aperiam atque.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Traveler;
